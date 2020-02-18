9a5f17ab10eef0f109e4e94fe69304307d2d534b
giacThePhantom <giacomo.fantoni@studenti.unitn.it>
2020-02-18 08:41:50 +0100

    Programmazione dinamica, lezione 17/02/20

---
+++ Codice/MemoizationAutomatica.py
@@ -0,0 +1,10 @@
+from functools import wraps
+
+def memo(func):
+    cache = {}
+    @wraps(func)
+    def wrap(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return wrap
